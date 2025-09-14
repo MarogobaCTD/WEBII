c1510b641aa91d191563e6dd4a335eae5f1fdcce
marogoba <marogoba@gmail.com>
2025-09-14 13:36:47 -0300

    Criação da pasta no github

---
+++ 2025/Semana1/Projeto1_IF/Projeto1_IF/Areas/Identity/Data/Projeto1_IFContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Projeto1_IF.Data;
+
+public class Projeto1_IFContext : IdentityDbContext<IdentityUser>
+{
+    public Projeto1_IFContext(DbContextOptions<Projeto1_IFContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
